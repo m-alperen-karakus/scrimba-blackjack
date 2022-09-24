32426b7833ca20ab44ab84162cb66e777dda9015
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-09-24 17:17:07 +0300

    almost finished

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let player1 =0
 let player2 =0
 let player3 =0
-
+let cardsSum = document.getElementById("cardsSum")
 function dispenseCards(sumCards) {
+    console.log(sumCards)
     let card1 = Math.floor(Math.random()*19) +1
     let card2 = Math.floor(Math.random()*19) +1
+
+    if (card1 > 10) { card1 =10 }
+    if (card2 > 10) { card2 =10 }
+
     return sumCards += card1+ card2
 }
 
-function pullCard(extraCard){
-    let card3 = Math.floor(Math.random()*19) +1
-    return extraCard += card3
 
+
+function pullCard(){
+    let card3 = Math.floor(Math.random()*19) +1
+    player1 += card3
+    cardsSum.textContent = "Summary of your desk: " + player1
 }
 
 function pullFromTable()
@@ -26,15 +33,37 @@ function pullFromTable()
     { return console.log("player 2 win the game") }
     else if ((player3 <=21) && (player3 > player2)  &&  (player3 > player1))
     { return console.log("player 3 win the game") }
-    else{
-    return console.log("nobody win the game")
+    
+    // bc of people which is lost cant compare their desk
+    else if (player1 > 21) { player1=-1 } 
+    else if (player2 > 21) { player2=-1 } 
+    else if (player3 > 21) { player3=-1 } 
+
+    else if ( player1===player2 || player1===player3 || player2===player3 || player1===player2 && player1===player3 )
+    {
+        console.log( "Nobody win the game bc of some of players have equall number with others" )
     }
+    else{
+        console.log("bug")
+    }
+
+
+    // reset of all decks burası buglı
+    player1=0
+    player2=0
+    player3=0
+
+    document.getElementById("start").hidden=false
+    document.getElementById("startText").textContent="Wanna play a round"
+    cardsSum.textContent = "Summary of your desk: "
 }
 
+function start(){
+    player1 = dispenseCards(player1)
+    player2 = dispenseCards(player2)
+    player3 = dispenseCards(player3)
+    cardsSum.textContent = "Summary of your desk: " + player1
+    document.getElementById("start").hidden=true
+    document.getElementById("startText").textContent="Game is running!!"
+}
 
-player1 = dispenseCards(player1)
-player2 = dispenseCards(player2)
-player3 =  dispenseCards(player3)
-
-pullCard(player1)
-pullFromTable()
