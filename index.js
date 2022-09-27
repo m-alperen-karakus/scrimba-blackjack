28c87d61ff1071841410c8a401fcf2312024ae22
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-09-28 01:08:37 +0300

    Blackjack finished

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let player1 =0
 let player2 =0
 let player3 =0
+
+//chips
+let chipsText= document.getElementById("chips")
+let chips=200
+
 let cardsSum = document.getElementById("cardsSum")
 function dispenseCards(sumCards) {
-    console.log(sumCards)
     let card1 = Math.floor(Math.random()*19) +1
     let card2 = Math.floor(Math.random()*19) +1
 
@@ -23,39 +27,44 @@ function pullCard(){
 
 function pullFromTable()
 {   
-    console.log(player1)
-    console.log(player2)
-    console.log(player3)
+    cardsSum.textContent = "Player 1: " + player1 +" Player 2: " + player2 + " Player 3: "+player3
 
-    if ((player1 <=21) && (player1 > player2)  &&  (player1 > player3)) 
-    { return console.log("player 1 win the game")}
-    else if ((player2 <= 21) && (player2 > player1)  &&  (player2 > player3))
-    { return console.log("player 2 win the game") }
-    else if ((player3 <=21) && (player3 > player2)  &&  (player3 > player1))
-    { return console.log("player 3 win the game") }
-    
     // bc of people which is lost cant compare their desk
-    else if (player1 > 21) { player1=-1 } 
+    if (player1 > 21) { player1=-1 } 
     else if (player2 > 21) { player2=-1 } 
     else if (player3 > 21) { player3=-1 } 
 
+    if ((player1 <=21) && (player1 > player2)  &&  (player1 > player3)) 
+    { 
+        chips +=50
+        document.getElementById("startText").textContent="Player 1 win the game"
+    }
+    else if ((player2 <= 21) && (player2 > player1)  &&  (player2 > player3))
+    { 
+        chips -=50
+        
+        document.getElementById("startText").textContent="Player 2 win the game" 
+    }
+    else if ((player3 <=21) && (player3 > player2)  &&  (player3 > player1))
+    { 
+        chips -=50
+        document.getElementById("startText").textContent="Player 3 win the game" 
+    }
+    
+
     else if ( player1===player2 || player1===player3 || player2===player3 || player1===player2 && player1===player3 )
-    {
-        console.log( "Nobody win the game bc of some of players have equall number with others" )
-    }
-    else{
-        console.log("bug")
-    }
+    { document.getElementById("startText").textContent="Nobody win the game bc of some of players have equall number with others" }
 
+    console.log(chips)
+    chipsText.textContent= "Total Chips: "+ chips +"$"
+    
 
-    // reset of all decks burası buglı
+    // reset of all decks
     player1=0
     player2=0
     player3=0
 
     document.getElementById("start").hidden=false
-    document.getElementById("startText").textContent="Wanna play a round"
-    cardsSum.textContent = "Summary of your desk: "
 }
 
 function start(){
